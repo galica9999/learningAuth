12e3b87ee47389907c3edc34d4e6c510e1fa7c3b
galica9999 <galica9999@gmail.com>
2020-09-13 18:17:10 -0400

    More packages
    Installed more packages to be used.  added page that will be hidden behind auth

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
-var express = require('express');
+var express                 = require('express'),
+    mongoose                = require('mongoose'),
+    passport                = require('passport'),
+    bodyParser              = require('body-parser'),
+    localStrategy           = require('passport-local'),
+    passportLocalMongoose   = require('passport-local-mongoose');
 
 
 var app = express();
 app.set('view engine', 'ejs');
 
+mongoose.connect('mongodb://localhost:27017/auth', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+    .then(() => console.log('Connected to DB!'))
+    .catch(error => console.log(error.message));
+
 app.get('/', function(req,res){
     res.render('home');
+});
+
+app.get('/secret', function(req,res){
+    res.render('secret');
 });
 
 app.listen(3000,'localhost', function(){
