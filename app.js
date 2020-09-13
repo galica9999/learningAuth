317e2a43ba088f0719925fecccafd6d709f48df3
galica9999 <galica9999@gmail.com>
2020-09-13 19:03:26 -0400

    login functionality
    added login functionality and routes

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use(bodyParser.urlencoded({extended:true}));
 
+passport.use(new localStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
@@ -59,6 +60,17 @@ app.post('/register', function(req,res){
             })
         }
     })
+});
+
+// login routes
+app.get('/login',function(req,res){
+    res.render('login');
+});
+
+app.post('/login', passport.authenticate('local', {
+    successRedirect: '/secret',
+    failureRedirect: '/login'
+}) ,function(req,res){
 });
 
 app.listen(3000,'localhost', function(){
